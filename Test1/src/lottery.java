933afd08e87190897e9431d94825745a48139e32
Ivy <chiuhanyi@gmail.com>
2020-02-21 10:28:14 +0800

    lottery

---
+++ Test1/src/lottery.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/* Ask user to input 6 numbers, numbers are between 1~49
+ * then randomly generates 6 winning numbers
+ * show the corresponding prize
+ *  6 numbers NT 2,000,000
+ *  5 numbers NT 200,000
+ *  4 numbers NT 20,000
+ *  3 numbers NT 200
+ */
+public class lottery {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter 6 numbers which between 1~49:");
+		Scanner scanner = new Scanner(System.in);
+		int UserNumbers = scanner.nextInt();
+
+	}
+}
